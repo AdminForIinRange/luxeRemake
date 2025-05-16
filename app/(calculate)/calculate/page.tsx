482dff1412722df168fef58d54a15af775c2f665
Anjesh <bhattaraianjesh123@gmail.com>
2025-05-16 18:06:14 +0930

    bot

--- app/(calculate)/calculate/page.tsx
+++ app/(calculate)/calculate/page.tsx
@@ -1,11 +1,12 @@
 import CaluProperty from '@/components/luxeComponents/CaluProperty'
+import GptLuxeCalc from '@/components/luxeComponents/GptLuxeCalc'
 import React from 'react'
 
 const Calculate = () => {
   return (
     <>
     
-    <CaluProperty /> 
+<GptLuxeCalc />
     
     </>
   )
