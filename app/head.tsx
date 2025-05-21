d0b8f2d267f38472aa9a6d9ef12768ee0244be13
Anjesh <bhattaraianjesh123@gmail.com>
2025-05-21 20:33:28 +0930

    adding more stuff to luxe

--- app/head.tsx
+++ app/head.tsx
@@ -22,7 +22,20 @@ export default function Head() {
       <meta name="twitter:title" content="Luxe Management | Airbnb Property Management Adelaide" />
       <meta name="twitter:description" content="Expert Airbnb property management in Adelaide. Luxe Management maximizes your rental income with full-service hosting, cleaning, styling, and guest care." />
       <meta name="twitter:image" content="https://www.luxemanagements.com/og-image.jpg" />
-
+ <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Luxe Management",
+            url: "https://www.luxemanagements.com",
+            description:
+              "Airbnb property management in Adelaide. Short-term and holiday rental services tailored to maximize your income.",
+            logo: "https://www.luxemanagements.com/logo.png",
+          }),
+        }}
+      />
       {/* Favicon */}
       <link rel="icon" href="/favicon.ico" />
 
