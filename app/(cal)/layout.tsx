8ee58597c9e7166cf33e7f50a9546bc1dd8b2e61
Anjesh <bhattaraianjesh123@gmail.com>
2025-05-24 21:35:10 +0930

    commenting out cal

--- app/(cal)/layout.tsx
+++ app/(cal)/layout.tsx
@@ -1,21 +1,21 @@
-// app/news/layout.tsx
+// // app/news/layout.tsx
 
-import React from "react";
-import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer";
-import FAQ from "@/components/FAQ";
+// import React from "react";
+// import Navbar from "@/components/Navbar/Navbar";
+// import Footer from "@/components/Footer";
+// import FAQ from "@/components/FAQ";
 
-export const dynamic = "force-dynamic"; // <-- every child route under /news
-export default function NewsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
+// export const dynamic = "force-dynamic"; // <-- every child route under /news
+// export default function NewsLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <>
+//       <Navbar />
+//       <main>{children}</main>
 
-    </>
-  );
-}
+//     </>
+//   );
+// }
